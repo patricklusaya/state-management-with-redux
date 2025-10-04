caf00484eab19b1dedea1312e98b2254a3da617b
patricklusaya <lusayapatrick@gmail.com>
2025-10-04 23:27:58 +0300

    Phase 2 changes

--- my-app-phase-2/src/redux/store.js
+++ my-app-phase-2/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from './slices/counterSlice'
+import usersReducer from "./slices/usersSlice";
 
 export const store = configureStore({
 
   reducer:{
     // todo: we will add content in here
-    counter:counterReducer 
+    counter:counterReducer,
+    users:usersReducer 
   }
 
 })
